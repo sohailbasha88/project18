1575270e8486e84f3c41729a5d16990ffd94e388
sohailbasha88 <sohailbasha88@gmail.com>
2023-12-08 18:07:41 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def mdb5(request):
+    return render(request,'mdb5.html')
+def mdbchild(request):
+    return render(request,'mdbchild.html')
